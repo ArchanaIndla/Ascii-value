89191f68f13ca010499b8f822e4171a07821c645
ArchanaIndla <indlaarchana4751@gmail.com>
2025-01-28 18:45:38 +0530

    Add files via upload

---
+++ Asciival.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Asciival {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any character: ");
+        Character ch=sc. next().charAt(0);
+        sc.close();
+        System.out.println((int)(ch)); 
+      }
+}
